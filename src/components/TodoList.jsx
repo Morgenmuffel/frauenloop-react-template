2bfd321b4de305b269911076cb5fe155e734d8c5
Yulia Vilensky <yulia@Yulias-MacBook-Pro.local>
2020-02-03 19:34:34 +0100

    Todo list: delete a todo implemented

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -14,7 +14,7 @@ class TodoList extends Component {
 
     //check if the input is an empty string or just spaces
     if(this.state.input.trim() !== '') {
-      
+
       // add a new todo to the list
       editedTodos.push({ content: this.state.input });
 
@@ -34,6 +34,32 @@ class TodoList extends Component {
     });
   };
 
+  handleDelete = (indexToDelete) => {
+    
+    //Method 1
+    // copy todo list
+    const editedTodos = [...this.state.todos];
+
+    //remove the todo at 'index'
+    editedTodos.splice(indexToDelete, 1);
+
+    //save the updated todo list to state
+    this.setState({
+      todos: editedTodos
+    })
+
+    //Method 2
+    // make a copy of the todo list, but without the clicked item
+    const editedTodosWithFilter = this.state.todos.filter( (item, index) =>  {
+      return indexToDelete !== index;
+    });
+
+    this.setState({
+      todos: editedTodosWithFilter
+    })
+
+  };
+
   render() {
     return (
       <>
@@ -47,9 +73,19 @@ class TodoList extends Component {
         </form>
         <ul>
           {this.state.todos.map((item, index) => (
-            <li key={index}>{item.content}</li>
+            <li key={index}>{item.content}
+              <span className="delete-container">
+                <button
+                  type="button"
+                  className="deleteButton"
+                  onClick={() => {
+                    this.handleDelete(index)
+                  }}>Delete</button>
+              </span>
+            </li>
           ))}
         </ul>
+
       </>
     );
   }
