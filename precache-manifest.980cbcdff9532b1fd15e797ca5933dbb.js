50bfc8077795922a265a1556438af9fc31877e31
Yulia Vilensky <yulia@Yulias-MacBook-Pro.local>
2020-02-03 20:11:29 +0100

    Updates

---
+++ precache-manifest.980cbcdff9532b1fd15e797ca5933dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada9cdf74d040ae561c8223d0a986ab0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5fdbe9f675df22ce7334",
+    "url": "./static/css/main.4a8404cb.chunk.css"
+  },
+  {
+    "revision": "a5cbd1ded5ca90dcff3f",
+    "url": "./static/js/2.bd09e5e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.bd09e5e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fdbe9f675df22ce7334",
+    "url": "./static/js/main.1a184823.chunk.js"
+  },
+  {
+    "revision": "6f1cb0f1d4d9c8208063",
+    "url": "./static/js/runtime-main.d6441537.js"
+  }
+]);
